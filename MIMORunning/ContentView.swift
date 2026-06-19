d7b7cf463db224b5fa058148ef81c048420246f5
hawkblue70-cmd <hawkblue70.cmd@gmail.com>
2026-06-19 19:18:38 +0900

    Initial Commit

---
+++ MIMORunning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MIMORunning
+//
+//  Created by 한남석 on 6/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MIMORunning/MIMORunningApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIMORunningApp.swift
+//  MIMORunning
+//
+//  Created by 한남석 on 6/19/26.
+//
+
+import SwiftUI
+
+@main
+struct MIMORunningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
